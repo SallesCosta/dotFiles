79f0cfd35bf630e66ea9410691a168b2b41f49de
SallesCosta <newcapital.in@gmail.com>
2023-01-11 13:06:28 +0100

    add: colorizer

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,37 @@
-local colorscheme = "tokyonight"
+-- Material Theme config
+--https://github.com/marko-cerovac/material.nvim
+vim.g.material_style = 'deep ocean' -- oceanic / deep ocean / palenight / lighter / darker
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  return
-end
+require('material').setup({
+  contrast = {
+    sidebars = true,
+    floating_windows = true,
+  },
+  italics = {
+    keywords = true,
+    functions = true,
+  },
+  contrast_filetypes = {
+    'terminal',
+    'packer',
+    'qf',
+  },
+  disable = {
+    borders = false,
+    eob_lines = false
+  },
+  lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+  async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+  custom_highlights = {} -- Overwrite highlights with your own
+})
+
+vim.cmd [[
+try
+  colorscheme nord " material | nightfox | dayfox | nordfox | dawnfox | duskfox | terafox  | nord | tokyon
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme nightfox 
+  set background=dark
+endtry
+]]
